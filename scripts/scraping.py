9b06a537f924a6fbfa3e2a1689733e57c66470d3
basleal22 <solibaslu@gmail.com>
2025-02-01 11:46:06 +0300

    modified the scraping module to include a csv converter function

--- scripts/scraping.py
+++ scripts/scraping.py
@@ -1,29 +1,37 @@
-from telethon import TelegramClient, events
-import asyncio
-# List of channels to scrape
-CHANNELS = [
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from dotenv import load_dotenv
+from telethon import TelegramClient
+import csv
+async def scraper(client, channel_username, writer):
+    entity = await client.get_entity(channel_username)
+    channel_title = entity.title  # Extract the channel's title
+
+    # Specifies to fetch up to 600 messages
+    async for message in client.iter_messages(entity, limit=60000):
+        # Write the channel title along with other data (excluding media handling)
+        writer.writerow([channel_title, channel_username, message.id, message.message, message.date])
+
+async def main(client):
+    await client.start()
+    
+
+    # Open the CSV file and prepare the writer
+    with open('telegram_data.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Channel Title', 'Channel Username', 'ID', 'Message', 'Date', 'Media Path'])  # Include channel title in the header
+        
+        # List of channels to scrape
+        channels = [
     'https://t.me/DoctorsET',
-    'https://t.me/ChemedTelegramChannel',
     'https://t.me/lobelia4cosmetics',
     'https://t.me/yetenaweg',
     'https://t.me/EAHCI'
-]
-async def scrape_tel(channel_link,client):
-    #Scrapes the last 1000 messages from a given Telegram channel
-    channel_username = channel_link.split('/')[-1]
-    channel = await client.get_entity(channel_username)
-    messages = await client.get_messages(channel, limit=10000)
+    ]        
+        # Iterate over channels and scrape data into the single CSV file
+        for channel in channels:
+            await scraper(client, channel, writer)
+            print(f"Scraped data from {channel}")
     
-    # Process and display messages
-    for message in messages:
-        if message.text:
-            print(f"Message from {channel_username}: {message.text}")
-async def main():
-    for channel in CHANNELS:
-        print(f"Scraping {channel}...")
-        await scrape_tel(channel)
-async def scrape_multiple_channels(client, channels):
-    """Scrapes messages from multiple Telegram channels."""
-    for channel in channels:
-        print(f"Scraping {channel}...")
-        await scrape_tel(client, channel)
